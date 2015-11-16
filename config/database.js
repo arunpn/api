8cd8c444fed1b6fa021035ff3c544209f25a98cc
Mauricio Giordano <mauricio.c.giordano@gmail.com>
2015-11-15 23:45:13 -0200

    Added support for full URL

--- config/database.js
+++ config/database.js
@@ -20,23 +20,36 @@ function database() {
         return struct
     }
 
-    struct.sequelize = new Sequelize(
-        process.env.CONN_DATABASE,
-        process.env.CONN_USERNAME,
-        process.env.CONN_PASSWORD,
-        {
-            host    : process.env.CONN_HOST,
-            dialect : process.env.CONN_DIALECT,
-            logging : false,
-
-            pool : {
-                max  : 5,
-                min  : 0,
-                idle : 10000
+    if (process.env.CONN_FULL_URL_USE == 'true') {
+        struct.sequelize = new Sequelize(
+            process.env.CONN_FULL_URL,
+            {
+                dialect : process.env.CONN_DIALECT,
+                logging : false,
+                pool : {
+                    max  : 5,
+                    min  : 0,
+                    idle : 10000
+                }
             }
-        }
-    )
-
+        )
+    } else {
+        struct.sequelize = new Sequelize(
+            process.env.CONN_DATABASE,
+            process.env.CONN_USERNAME,
+            process.env.CONN_PASSWORD,
+            {
+                host    : process.env.CONN_HOST,
+                dialect : process.env.CONN_DIALECT,
+                logging : false,
+                pool : {
+                    max  : 5,
+                    min  : 0,
+                    idle : 10000
+                }
+            }
+        )
+    }
     struct.generated = true
 
     return struct
